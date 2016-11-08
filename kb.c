739b04b6f0023717187b228d0136b1cad7d1d227
Leo Smith <k0oo@icloud.com>
2016-11-08 14:05:36 +0100

    :floppy_disk:

---
+++ kb.c
@@ -0,0 +1,81 @@
+/*
+ * /$$                           /$$                                           /$$              
+ *| $$                          | $$                                          | $$              
+ *| $$   /$$  /$$$$$$  /$$   /$$| $$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$      /$$$$$$$
+ *| $$  /$$/ /$$__  $$| $$  | $$| $$__  $$ /$$__  $$ |____  $$ /$$__  $$ /$$__  $$     /$$_____/
+ *| $$$$$$/ | $$$$$$$$| $$  | $$| $$  \ $$| $$  \ $$  /$$$$$$$| $$  \__/| $$  | $$    | $$      
+ *| $$_  $$ | $$_____/| $$  | $$| $$  | $$| $$  | $$ /$$__  $$| $$      | $$  | $$    | $$      
+ *| $$ \  $$|  $$$$$$$|  $$$$$$$| $$$$$$$/|  $$$$$$/|  $$$$$$$| $$      |  $$$$$$$ /$$|  $$$$$$$
+ *|__/  \__/ \_______/ \____  $$|_______/  \______/  \_______/|__/       \_______/|__/ \_______/
+ *                     /$$  | $$                                                               
+ *                    |  $$$$$$/                                                               
+ *                     \______/                                          
+ * Keyboard programs for arduino :P
+ */
+
+#include "keys.h"
+#define BUFSIZ 1024
+
+uint8_t key[8] = {
+	0 };
+
+enum { HOLD = 1, DONT_HOLD = 0 };
+
+void kb_init()
+{
+	Serial.begin(9600);
+	delay(200);
+}
+
+void releaseKey()
+{
+	key[0] = 0;
+	key[2] = 0;
+	key[4] = 0;
+	Serial.write(key, 8);
+}
+
+void writeKey(int lettr, int hold, int attr)
+{
+	key[4] = lettr;
+	key[2] = attr;
+	Serial.write(key, 8);
+	if(!(hold))
+		releaseKey();
+}
+
+void printKey(char str[BUFSIZ])
+{
+	int temp;
+	for( int i = 0; str[i] != '\0'; i++) {
+		temp = 0;
+    if((97 <= str[i]) && (str[i] <= 122)) {
+      temp = str[i] - 93;     //93
+      writeKey(temp, 0, 0);
+    } else if((49 <= str[i]) && (str[i]<= 57)) {  // 1 - 9
+      temp = str[i] - 19;
+      writeKey(temp, 0, 0);
+    } else if( (str[i] == 58) || (str[i] == 59) ) {
+			temp = KB_SEMICOLON;
+      if( !(str[i] == 59) ){ // if :
+      	writeKey(temp, 0, SHIFT);
+			} else {
+				writeKey(temp, 0, 0);
+			}
+    } else if( (str[i] == 47) || (str[i] == 63)) {
+      temp = KB_FORWARD_SLASH;
+			if(!(str[i] == 47)){
+      	writeKey(temp, 0, SHIFT);
+			} else {
+				writeKey(temp, 0, 0);
+			}
+    } else if((str[i] == 46) || (str[i] == 62)) {
+			temp = KB_DOT;
+      if(!(str[i] == 46)) {
+        writeKey(temp, 0, SHIFT);
+			} else {
+				writeKey(temp, 0, 0);
+			}
+    }
+	}
+}
---
+++ keys.h
@@ -0,0 +1,94 @@
+/*
+ * /$$   /$$                                  /$$      
+ *| $$  /$$/                                 | $$      
+ *| $$ /$$/   /$$$$$$  /$$   /$$  /$$$$$$$   | $$$$$$$ 
+ *| $$$$$/   /$$__  $$| $$  | $$ /$$_____/   | $$__  $$
+ *| $$  $$  | $$$$$$$$| $$  | $$|  $$$$$$    | $$  \ $$
+ *| $$\  $$ | $$_____/| $$  | $$ \____  $$   | $$  | $$
+ *| $$ \  $$|  $$$$$$$|  $$$$$$$ /$$$$$$$//$$| $$  | $$
+ *|__/  \__/ \_______/ \____  $$|_______/|__/|__/  |__/
+ *                     /$$  | $$                       
+ *                    |  $$$$$$/                       
+ *                     \______/  
+ * Made by papi
+ * The 7/11/2016
+ * Last modified the 7/11/2016
+ *****/
+
+#define KB_ERROR_ROLL_OVER 1
+#define KB_POSTFail 2
+#define KB_ERROR_UNDEFINED 3
+#define KB_A 4
+#define KB_B 5
+#define KB_C 6
+#define KB_D 7
+#define KB_E 8
+#define KB_F 9
+#define KB_G 10
+#define KB_H 11 
+#define KB_I 12
+#define KB_J 13
+#define KB_K 14
+#define KB_L 15
+#define KB_M 16
+#define KB_N 17
+#define KB_O 18
+#define KB_P 19
+#define KB_Q 20
+#define KB_R 21
+#define KB_S 22
+#define KB_T 23
+#define KB_U 24
+#define KB_V 25
+#define KB_W 26
+#define KB_X 27
+#define KB_Y 28
+#define KB_Z 29
+#define KB_1 30
+#define KB_2 31
+#define KB_3 32
+#define KB_4 33
+#define KB_5 34
+#define KB_6 35
+#define KB_7 36
+#define KB_8 37
+#define KB_9 38
+#define KB_0 39
+#define KB_ENTER 40
+#define KB_ESCAPE 41
+#define KB_DELETE 42
+#define KB_TAB 43
+#define KB_SPACE 44
+#define KB_DASH 45
+#define KB_EQUAL 46
+#define KB_L_BRAC 47
+#define KB_R_BRAC 48
+#define KB_BACKSLASH 49
+#define KB_TILDA 50
+#define KB_SEMICOLON 51
+#define KB_QUOTE 52
+#define KB_COMA 54
+#define KB_DOT 55
+#define KB_FORWARD_SLASH 56
+#define KB_CAPS_LOCK 57
+#define F1 58
+#define F2 59
+#define F3 60
+#define F4 61
+#define F5 62
+#define F6 63 
+#define F7 64
+#define F8 65 
+#define F9 66
+#define F10 67
+#define F11 68
+#define F12 69
+#define KB_PRINTSCREEN 70
+#define KB_R_ARROW 79
+#define KB_L_ARROW 80
+#define KB_D_ARROW 81
+#define KB_U_ARROW 82
+#define WINDOWS 227
+#define CTRL 224
+#define ALT 226
+#define SHIFT 225
