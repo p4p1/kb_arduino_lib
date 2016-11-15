1e73bcc96f97cfdec00ff816330191464e6040e7
Leo Smith <k0oo@icloud.com>
2016-11-15 20:24:05 +0100

    :floppy_disk:

---
+++ mac.h
@@ -0,0 +1,3 @@
+#include <kb.h>
+#define CMDSPACE writeKey(WINDOWS, 1, 0); \
+			writeKey(KB_SPACE, 0, WINDOWS);
---
+++ windows.h
@@ -0,0 +1,24 @@
+#include <kb.h>
+#include <keys.h>
+#define WINR writeKey(WINDOWS, 1, 0); \
+		writeKey(KB_R, 0, WINDOWS)
+#define CTRLESC writeKey(CTRL, 1, 0); \
+		writeKey(KB_ESCAPE, 0, CTRL)
+#define SYSMENU writeKey(ALT, 1, 0); \
+		writeKey(KB_SPACE, 0, ALT)
+#define DESKTOP writeKey(WINDOWS, 1, 0); \
+		writeKey(KB_D, 0, WINDOWS)
+#define CRTLS writeKey(CTRL, 1, 0); \
+		writeKey(KB_S, 0, CTRL)
+#define ALTF4 writeKey(ALT, 1, 0); \
+		writeKey(F4, 0, ALT)
+#define EXPLORER writeKey(WINDOWS, 1 ,0); \
+		writeKey(KB_E, 0, WINDOWS)
+#define CTRLF writeKey(CTRL, 1, 0); \
+		writeKey(KB_F, 0, CTRL)
+#define ALTO writeKey(ALT, 1, 0); \
+		writeKey(KB_O, 0, ALT)
+#define ALTTAB writeKey(ALT, 1 ,0); \
+		writeKey(KB_TAB, 0, ALT)
+#define WIND writeKey(WINDOWS, 1, 0); \
+		writeKey(KB_D, 0, WINDOWS)
